8078eade5820e0a27656ad75e265e634de1fb5f9
JennaMTucker <jenna.m.tucker@gmail.com>
2018-05-14 23:43:14 -0700

    new keys files

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://jmtucker7:12MERN17@ds251849.mlab.com:51849/traversymern',
-    secretOrKey: 'secret'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
 }
